c05d2b34018241696349226e57d65e32ef2d325e
Mert Üşenmez <33881395+MertUsenmez@users.noreply.github.com>
2019-04-25 03:18:31 +0300

    Update Message.cs

--- sms_mvc_project/Models/Message.cs
+++ sms_mvc_project/Models/Message.cs
@@ -29,17 +29,6 @@ namespace sms_mvc_project.Models
         [BsonElement("CreateOn")]
         public DateTime CreateOn { get; set; } = DateTime.Now;
 
-        /*
-          
-        Conversation : {
-          id: 123,
-          members: [ user_id1, user_id2 ]
-          }
 
-          Message { conversationId: 123, author: user_2, body: 'Hi what's up' }
-          
-        Message { conversationId: 123, author: user_1, body: 'Whanna ask some question on stackoverflow' }
-        
-         */
     }
 }
